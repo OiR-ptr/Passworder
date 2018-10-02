52947ba06cc9a7bffa1ff3bfa9a3995fa6ce0720
Unknown <myr_ryusuke@hotmail.co.jp>
2018-10-03 00:36:14 +0900

    :+1: fix register icon
    登録アイコンが触れなくなっていたため修正

--- src/components/TagList.jsx
+++ src/components/TagList.jsx
@@ -9,27 +9,43 @@ import IconButton from "@material-ui/core/IconButton";
 import PlaylistAdd from "@material-ui/icons/PlaylistAdd";
 
 export default class TagList extends React.Component {
-    render() {
-        var list = [];
+    constructor(props) {
+        super(props);
+
+        this.list = [];
         this.props.tags.forEach(element => {
-            list.push(
+            this.list.push(
                 <Tag key = {element.tagID} style={{ width: "100%" }} tagID = {element.tagID} title = {element.title} abstract = {element.abstract} />
             );
         });
+    }
 
+    componentWillUpdate(nextProps) {
+        this.list = [];
+        nextProps.tags.forEach(element => {
+            this.list.push(
+                <Tag key = {element.tagID} style={{ width: "100%" }} tagID = {element.tagID} title = {element.title} abstract = {element.abstract} />
+            );
+        });
+    }
+
+    render() {
         return (
             <GridList cellHeight = {"auto"} cols = {1} style = {{ flexDirection: "column", flexWrap: "nowrap", height: "100%" }}>
                 <GridTile key = "Subheader" cols = {1} rows = {1} >
-                    <ListSubheader component="div">REGISTER</ListSubheader>
-                    <GridListTileBar actionIcon = { 
-                        <IconButton onClick = { (e) => { 
-                            this.props.addTag(this.props.tagID + 1) 
-                        }} >
-                            <PlaylistAdd />
-                        </IconButton> 
-                    } />
+                    <ListSubheader component="div">
+                        REGISTER
+                        <GridListTileBar actionIcon = { 
+                            <IconButton onClick = { (e) => { 
+                                console.log("push the tempo!");
+                                this.props.addTag(this.props.tagID + 1) 
+                            }} >
+                                <PlaylistAdd />
+                            </IconButton> 
+                        } />
+                    </ListSubheader>
                 </GridTile>
-                {list}
+                {this.list}
             </GridList>
         );
     }
